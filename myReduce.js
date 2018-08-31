da9870982aef2505e6fa680879f061233fae9661
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-08-31 05:54:39 +0000

    minor edits

--- myReduce.js
+++ myReduce.js
@@ -12,32 +12,37 @@ function myReduce(arr, cb, initVal){
   for(var i = initVal !== undefined ? 0 : 1; i < arr.length; i++){
     result = cb(result, arr[i]);
   }
-  
+
   return result;
 }
 
 console.log(myReduce(testArr, sum));
-console.log(myReduce(testArr2, joiner));
+console.log(myReduce(testArr2, stringer));
 console.log(myReduce(testArr3, obJoiner, ""));
 
 
-function max(maxSoFar, next) {
-  return maxSoFar > next ? maxSoFar : next;
+function findMax(currMax, next) {
+  return currMax > next ? currMax : next;
 }
 
-function sum(accumulator, next) {
-  return accumulator + next;
+function sum(acc, next) {
+  return acc + next;
 };
 
-function sumEvens(accumulator, next) {
-  return next % 2 === 0 ? accumulator + next : accumulator;
+function sumEvens(acc, next) {
+  return next % 2 === 0 ? acc + next : acc;
 }
 
-function joiner(acc, next) {
-  var joint = ' ';
-  return Array.isArray(next) ? acc + myReduce(next, joiner) : acc + joint + next;
+function sumOdds(acc, next){
+  return next % 2 === 1 ? acc + next : acc;
+}
+
+function stringer(acc, next) {
+  var joint = ', ';
+  return Array.isArray(next) ? acc + myReduce(next, stringer) : acc + joint + next;
 };
 
 function obJoiner(acc, next){
-  return acc + Object.values(next).join(", ");
+  var joint = " ";
+  return acc + (Object.values(next) + joint);
 }
